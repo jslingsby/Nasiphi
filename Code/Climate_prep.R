37597a9063fe2fbd8b8e1096448ef8fde96e966f
jslingsby <jslingsby@gmail.com>
2016-05-04 14:46:21 +0200

    Fiddling with data setup

--- Code/Climate_prep.R
+++ Code/Climate_prep.R
@@ -13,45 +13,45 @@ library(rgdal)
 library(raster)
 
 if(Sys.getenv("USERNAME")=="Receptionist") {datwd <- "C:/Users/Receptionist/Dropbox/Academics/PhD/Data/ClimateData/"}
-if(Sys.getenv("USER")=="jasper") {datwd <- ""}
+if(Sys.getenv("USER")=="jasper") {datwd <- "/Users/jasper/Documents/GIS/VegToolsRaw/Rasters/"; climwd <- "/Users/jasper/GIT/Nyasha/Data/Adam/"}
 
 ##########################################
 ###2) Get and process data
 ##########################################
 
-c <- brick(paste(datwd,"currentclimate.grd", sep = ""))
-f <- brick(paste(datwd,"futureclimate.grd", sep = ""))
+###Get climate data
+c <- brick(paste(climwd,"currentclimate.grd", sep = ""))
+f <- brick(paste(climwd,"futureclimate.grd", sep = ""))
 
-
-#extract layers to use in calculation
-#c[[which(names(c)%in%c("map","tmax01"))]]
-tmax <- raster(c,layer="tmax01")
-tmin <- raster(c,layer="tmin07")
-mmp <- raster(c,layer="mmp01")
-tmaxf <- raster(f,layer="RCP45_bcccsm11_20812100_tmax01.1")
-tminf <- raster(f,layer="RCP45_bcccsm11_20462065_tmin07.1")
-mmpf <- raster(f,layer="RCP45_bcccsm11_20812100_mmp01.1")
-
-#write ascii grid for analysis
-writeRaster(tmax, filename = "tmax.asc", format="ascii")
-writeRaster(tmin, filename = "tmin.asc", format="ascii")
-writeRaster(mmp, filename = "mmp.asc", format="ascii")
-writeRaster(mmpf, filename = "mmpf.asc", format="ascii")
-writeRaster(tmaxf, filename = "tmaxf.asc", format="ascii")
-writeRaster(tminf, filename = "tminf.asc", format="ascii")
-
-#need raster with planning units
- 
-pa <- raster("C:/Users/Receptionist/Dropbox/Academics/PhD/Data/Rasters/PA_npaes_test_web.tif")
+###Get a raster with planning units based on the protected areas
+pa <- raster(paste(datwd, "PA_npaes_test_web.tif", sep=""))
 proj4string(pa) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")
-vm <- raster("C:/Users/Receptionist/Dropbox/Academics/PhD/Data/Rasters/VEG12test_web.tif")
+vm <- raster(paste(datwd, "VEG12test_web.tif", sep=""))
 proj4string(vm) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")
 
-#reproject 
+###Reproject protected areas and climate data to the same projection, crop, and rasterize to the same grid size
 proj4string(c)
+proj4string(f)
 proj4string(pa)
-#pa and c are in different projections...
-paC <- projectRaster(pa, crs = CRS(proj4string(c)))
+#pa, f and c are in different projections...
+
+#extract layers to use in calculation
+rsh <- c("tmax01", "tmin07", "mmp01") #historical
+his <- c[[which(names(c)%in%rsh)]]
+rsf <- c("RCP45_bcccsm11_20812100_tmax01.1", "RCP45_bcccsm11_20462065_tmin07.1", "RCP45_bcccsm11_20812100_mmp01.1") #future
+fut <- f[[which(names(f)%in%rsf)]]
+
+#trim protected areas to the same extent as the climate data
+cE <- extent(projectRaster(his, crs = CRS(proj4string(pa)))) #Get the extent of c when reprojected to the same coord system as pa
+pa <- crop(pa, cE) #Crop pa to the extent of c
+
+#reproject and rasterize historical climate to 30m and write out
+projectRaster(his, pa, filename = "Data/historical", bylayer = T, suffix = names(his)) 
+
+#reproject and rasterize historical climate to 30m and write out
+projectRaster(fut, pa, filename = "Data/future", bylayer = T, suffix = names(fut)) 
+
+x <- raster(list.files("Data/", filename = "Data/future", bylayer = T, suffix = names(his), bylayer=T, format="ascii")
 
 ############################################################
 ##### 3) Climate stability calculation
