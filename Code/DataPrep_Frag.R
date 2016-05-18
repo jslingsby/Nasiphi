6a22c1112399092e5b212db2bbd9b113c8a78749
unknown <jasper@CPTDSK0334.saeon.local>
2016-05-18 15:21:38 +0200

    nasiphi playing with patch statistics for each vegetation type

--- Code/DataPrep_Frag.R
+++ Code/DataPrep_Frag.R
@@ -19,7 +19,7 @@ library(igraph)
 
 if(Sys.getenv("USERNAME")=="Receptionist") {datwd <- "C:/Users/Receptionist/Dropbox/Academics/PhD/Data/"}
 if(Sys.getenv("USER")=="jasper") {datwd <- "/Users/jasper/Documents/GIS/VegToolsRaw/"}
-if(Sys.getenv("USERNAME")=="jasper") {datwd <- ""}
+if(Sys.getenv("USERNAME")=="jasper") {datwd <- "~/Nasiphi's/Data/"}
 ##########################################
 ###2) Get and process data
 ##########################################
@@ -54,39 +54,61 @@ tab <-lc@data@attributes[[1]]
 tb <- tab[,c("Value","LC3V")]
 tb <- rbind(tb, c(128,"NA"))
 tb <- rbind(tb, c(0,"NA"))
-subs(lc,tab, by="ID",which="LC3V",subsWithNA=TRUE, filename='lcs', overwrite=T) #reclassifies raster to 1s and 0s
-lc <- raster("Data/lcs.grd")
 
+if(!file.exists("Data/lcs.grd")) {
+  subs(lc,tab, by="ID",which="LC3V",subsWithNA=TRUE, filename= "Data/lcs", overwrite=T)
+} else {lc <- raster("Data/lcs.grd")}
 
-## mask veg map based on untransformed landcover to get fragments layer
-fragments <- mask(vm, lc, filename="Data/vmt", maskvalue= 0, overwrite=TRUE)
+#create a transformed veg map
+if(!file.exists("Data/vmt.grd")) {
+  veg <- mask(vm, lc, filename="Data/vmt", maskvalue= 0, overwrite=TRUE)
+} else {veg <- raster("Data/vmt.grd")}
 
-##make map for each veg type and get patch stats within eachveg type
-vegtab <- fragments@data@attributes[[1]]
+### create a raster layer for each veg type
+# find a loop function that creates this
 
-#extract by vegtype names
-#use which function  
+##HAVE TO AGGREGATE EACH RASTER
+vtab <- vm@data@attributes[[1]]
+
+veg2 <- veg==2 #veg type 2 only
+aggregate(veg2, fact=3, fun=mean, expand=TRUE, na.rm=TRUE, filename='veg2ag')
+veg2ag <- raster("veg2ag.grd")
+veg59 <- veg ==59
+
 
 #########################################
 ##### 2) Fragmentation analysis #########
 ##########################################
 
-#calculate fragstats for remnant natural land cover
+#Class statistics for different veg types
+classfrag <- ClassStat(veg, cellsize = 30)
 
-#fragmentation of all veg types
-classfrag = ClassStat(fragments)
-
-#patch stats for each veg type
-#[need to subset each veg type]
-
+#patch stats for each veg type #[need to subset each veg type]
 
 #landcover
 conlc = ConnCompLabel(lc) #identifies disjunct patches
 lcpatch = PatchStat(conlc,cellsize = 30) #calculate patch statistics
 colnames(lcpatch)[1] <- "ID"
 levels(conlc) <- lcpatch
-lcclass = ClassStat(conlc) #calculate class statistics
 
+#vegtype 2
+conv2 = ConnCompLabel(veg2)
+v2patch = PatchStat(conv2,cellsize = 30) #calculate patch statistics
+colnames(v2patch)[1] <- "ID"
+levels(conv2) <- v2patch
+
+
+#vegtype 2 aggregated to 90m
+conv2a = ConnCompLabel(veg2ag)
+v2apatch = PatchStat(conv2a,cellsize = 90) #calculate patch statistics
+colnames(v2apatch)[1] <- "ID"
+levels(conv2a) <- v2apatch
+
+#vegtype 59
+conv59 = ConnCompLabel(veg59)
+v59patch = PatchStat(conv59,cellsize = 30) #calculate patch statistics
+colnames(v59patch)[1] <- "ID"
+levels(conv59) <- v59patch
 
 #########################################
 ##### 3) Transformation analysis #########
